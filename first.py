4e097bade710dc5af4785b0a3576f45a060863a6
Sunidhi-Pradhan <sunidhipradhan16@gmail.com>
2025-09-03 22:55:34 +0530

    Refactor first.py to improve formatting and add missing print statements

--- first.py
+++ first.py
@@ -2,6 +2,11 @@ print("Hello sunidhi")
 s="Sunidhi"
 print(s*3);
 print(type(s));
+t = " 52"
+print(s+t)
+print(type(s+t));
+
+
 a = 10
 print(type(a));
 print(id(a));
@@ -9,3 +14,18 @@ x,y,z = 1,2,33
 print(x);
 print(y);
 print(z);
+
+# loops in python 
+for i in range(5):
+    print("number:",i)
+
+#for loop in an array
+fruits = ['apple','banana','mango']
+for  fruit in fruits:
+    print("i like :",fruit)
+
+# while loop
+i = 1
+while i <=5:
+    print(i)
+    i+=1
